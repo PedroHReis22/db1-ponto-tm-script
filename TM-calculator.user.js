18a25965076f3eef25bc8c5ce741b8ae3b98dbcb
Pedro Henrique Reis <pedro.hreis22@gmail.com>
2016-02-11 14:32:08 -0200

    Update TM-calculator.user.js

--- TM-calculator.user.js
+++ TM-calculator.user.js
@@ -10,5 +10,5 @@
 // ==/UserScript==
 
 $.getScript( "http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js" ).done(function() {
-    $.getScript("https://cdn.rawgit.com/PedroHReis22/db1-ponto-tm-script/master/db1-calculator.js");
+    $.getScript("https://cdn.rawgit.com/PedroHReis22/db1-ponto-tm-script/dfaef6f924907a16b5e5e8a12b0d1cb5fee6cfdc/db1-calculator.js");
 });
